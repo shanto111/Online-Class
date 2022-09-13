e9126e104431eaaa76e83de7acb6c50cf905e109
Shanto <mdhasibulhasan165@gmail.com>
2022-09-13 16:20:16 +0600

    first commit

---
+++ connection.php
@@ -0,0 +1,28 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "test";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password,$dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: ");
+}
+echo "successfully";
+
+$sql="INSERT INTO course(dates,c_course,add_Link,C_Status)VALUES('$_POST[datetime]','$_POST[course]','$_POST[addlink]','$_POST[status]')";
+
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+  
+} 
+else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
